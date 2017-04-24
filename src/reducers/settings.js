643afd679dd5c677733a4841c5ead58ec125944c
Evan Jones <evanjones4040@gmail.com>
2017-04-23 23:29:55 -0700

    adding square colors per theme. Done

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -3,28 +3,28 @@ const initialState = {
   themeOptions: ["Dark", "Light", "Oceanic", "Solarized"],
   gameColors: {
     Dark: {
-      red: "red",
-      blue: "blue",
-      orange: "orange",
-      green: "green"
+      red: "#B71C1C",
+      blue: "#0D47A1",
+      orange: "#E65100",
+      green: "#1B5E20"
     },
     Light: {
-      red: "red",
-      blue: "blue",
-      orange: "orange",
-      green: "green"
+      red: "#E53935",
+      blue: "#1E88E5",
+      orange: "#FB8C00",
+      green: "#43A047"
     },
     Oceanic: {
-      red: "red",
-      blue: "blue",
-      orange: "orange",
-      green: "green"
+      red: "#cc1a45",
+      blue: "#66cce6",
+      orange: "#ec7e46",
+      green: "#007c57"
     },
     Solarized: {
-      red: "red",
-      blue: "blue",
-      orange: "orange",
-      green: "green"
+      red: "#992e30",
+      blue: "#007a8c",
+      orange: "#cc7533",
+      green: "#42765b"
     }
   },
   username: "",
