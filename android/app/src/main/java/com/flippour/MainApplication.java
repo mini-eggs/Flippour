02442051f378e2cc31a0504807933448a5c9570a
Evan Jones <evanjones4040@gmail.com>
2017-04-29 13:26:07 -0700

    android is building again

--- android/app/src/main/java/com/flippour/MainApplication.java
+++ android/app/src/main/java/com/flippour/MainApplication.java
@@ -3,9 +3,8 @@ package com.flippour;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.idehub.Billing.InAppBillingBridgePackage;
-import com.zmxv.RNSound.RNSoundPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new InAppBillingBridgePackage(),
-            new RNSoundPackage(),
-            new RNDeviceInfo()
+            new RNDeviceInfo(),
+            new RNSoundPackage()
       );
     }
   };
