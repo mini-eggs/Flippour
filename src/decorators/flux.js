9c85425e32d69027c7b226417f746648450749a8
Evan Jones <evanjones4040@gmail.com>
2017-05-02 22:15:13 -0700

    adding optional typescript support

--- src/decorators/flux.js
+++ src/decorators/flux.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Actions } from "react-native-router-flux";
 
-function restartApplication() {
-  RestartApplication.Restart();
-}
-
 export function FluxDecorator() {
   return DecoratedComponent => props => (
     <DecoratedComponent {...props} {...Actions} />
